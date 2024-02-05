8098204b4c9d9666a52225fdeac1863f8126c697
XiaoJiang <jjm1837080396@gmail.com>
2024-02-05 12:31:18 +0800

    update

--- TKFtool.py
+++ TKFtool.py
@@ -12,6 +12,11 @@ class ItemsBox():
         self.Items=dict()
         self.Position=ps
 
+    def setPosition(self,ps:list) -> None:
+        self.Position=ps
+    def getPosition(self) -> list:
+        return self.Position
+
     def addItem(self,name,item:pygame.Surface,ps:list) -> None:
         '''添加组件
         name: 组件名称
@@ -22,7 +27,8 @@ class ItemsBox():
 
     def getItemPosition(self,itemname) -> list|None:
         '''获取组件绝对坐标
-        itemname: 组件名称
+        Args:
+            itemname: 组件名称
         return: 组件绝对坐标
         '''
         try:
@@ -34,15 +40,15 @@ class ItemsBox():
             print(traceback.format_exc())
             return None
 
-    def shader(self,target:pygame.Surface,ps:list) -> None:
+    def shader(self,target:pygame.Surface) -> None:
         '''渲染组件
         target: 目标组件
         ps: 组件盒子坐标
         '''
-        self.Position=ps
-        target.blit(self.RootItem,ps)
-        for item in self.Items.keys
-            target.blit(item[0],)
+
+        target.blit(self.RootItem,self.Position)
+        for key in self.Items.keys():
+            target.blit(self.Items[key][0],self.getItemPosition(key))
 
 
 if __name__ == '__main__':
@@ -51,15 +57,22 @@ if __name__ == '__main__':
     MainWindow=pygame.display.set_mode((800,600))
     pygame.display.set_caption("test")
     img= pygame.image.load('./test.png')
-
+    it=ItemsBox(img,[10,10])
+    it.addItem("test",img,(10,10))
     x=0
-    while True:
+
+    running=True
+    while running:
+        clock.tick(60)
         #事件处理
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()
+                running=False
         #界面绘制
         MainWindow.fill((36, 40, 59))
         MainWindow.blit(img,(x,0))
+        it.setPosition([x,0])
+        it.shader(MainWindow)
+        x+=1
         pygame.display.update()
-        clock.tick(60)
+pygame.quit()
